0d93d5827698d8f6fe4cb688fad55d2e20e9628c
Ruben Graf <iamsuperlative@gmail.com>
2025-03-04 11:45:06 +0100

    init

---
+++ lightsail-compose.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# fix interactive install
+export DEBIAN_FRONTEND=noninteractive
+
+# install latest version of docker the lazy way
+curl -sSL https://get.docker.com | sh
+
+# make it so you don't need to sudo to run docker commands
+usermod -aG docker ubuntu
+
+# install docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+mkdir -p /srv
+chown ubuntu -R /srv
+
+# Create Docker Compose service file
+cat <<EOF | sudo tee /etc/systemd/system/docker-compose-app.service
+[Unit]
+Description=Docker Compose Application Service
+Requires=docker.service
+After=docker.service
+
+[Service]
+Type=oneshot
+RemainAfterExit=yes
+# match the below to wherever you copied your docker-compose.yml
+WorkingDirectory=/srv/docker
+EnvironmentFile=/etc/environment
+ExecStart=/usr/local/bin/docker-compose up -d
+ExecStop=/usr/local/bin/docker-compose down
+TimeoutStartSec=0
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Reload systemd to recognize the new service
+sudo systemctl daemon-reload
+
+# Enable the service to start on boot
+sudo systemctl enable docker-compose-app
---
+++ readme.md
@@ -0,0 +1,15 @@
+All credits to https://github.com/hashdog/lightsail-docker-compose
+
+
+Enter the following code into the dialog box:
+
+   ```
+   curl -o lightsail-compose.sh https://raw.githubusercontent.com/hashdog/lightsail-docker-compose/master/lightsail-compose.sh
+
+   chmod +x ./lightsail-compose.sh
+
+   ./lightsail-compose.sh
+   ```
+
+
+sudo systemctl restart docker-compose-app
